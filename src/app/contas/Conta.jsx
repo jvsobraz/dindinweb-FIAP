5a4f3834fda26ee7e141ad023104545f91319187
jvsobraz <jvsobraz@hotmail.com>
2023-09-05 09:28:11 -0300

    Fetch

---
+++ src/app/contas/Conta.jsx
@@ -0,0 +1,19 @@
+import { BanknotesIcon } from '@heroicons/react/24/outline'
+import DropMenu from "@/components/DropMenu";
+
+export default function Conta({conta}) {
+    return (
+        <div id="data-row" className="group flex justify-between items-center p-2 my-1 rounded cursor-pointer hover:bg-slate-700">
+            <div className="flex gap-2">
+                <BanknotesIcon className="h-6 w-6" />
+                <span>{conta.nome}</span>
+            </div>
+            <div className="flex items-center">
+                <span>R$ 100.00</span>
+                <div className='invisible group-hover:visible'>
+                    <DropMenu />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/contas/page.js
+++ src/app/contas/page.js
@@ -1,12 +1,27 @@
 import NavBar from "@/components/NavBar";
+import Conta from "./Conta";
 
-export default function Contas() {
+async function getContas(){
+  const url = "http://localhost:8080/api/contas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
+
+export default async function Contas() {
+
+  const data = await getContas()
+
   return (
     <>
       <NavBar active={"contas"} />
 
-      <main>
-        <h1>Contas</h1>
+      <main className="bg-slate-900 mt-10 m-auto max-w-lg p-8 rounded">
+        <h1 className="text-2xl">Contas</h1>
+
+        <div id="data">
+          {data.map(conta => <Conta key={conta.id} conta={conta} />)}
+        </div>
+
       </main>
     </>
 
