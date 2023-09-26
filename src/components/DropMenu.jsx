8fdeae79f6e5bc9aace0d802e0f5810547056f30
jvsobraz <jvsobraz@hotmail.com>
2023-09-26 11:38:07 -0300

    Finished

--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -1,18 +1,49 @@
 "use client"
 
-import * as React from 'react';
+import { destroy } from '@/actions/contas';
+import useToast from '@/hooks/useToast';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import * as React from 'react';
 
-export default function DropMenu() {
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useRouter } from 'next/navigation';
+
+
+export default function DropMenu({ conta }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  const { error, success } = useToast()
+  const { push } = useRouter()
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
+  };
+  
+  const handleEdit = () => {
+    setAnchorEl(null);
+    push(`/contas/${conta.id}/edit`)
+  }
+
+  const handleDelete = async () => {
+    setAnchorEl(null);
+    const resp = await destroy(conta.id)
+    resp?.error ? error(resp.error) : success("Conta apagada com sucesso")
   };
 
   return (
@@ -25,7 +56,7 @@ export default function DropMenu() {
         onClick={handleClick}
         className='text-slate-300'
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6" /></svg>
       </Button>
       <Menu
         id="basic-menu"
@@ -36,8 +67,28 @@ export default function DropMenu() {
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItem onClick={handleClose}>editar</MenuItem>
-        <MenuItem onClick={handleClose}>apagar</MenuItem>
+        <AlertDialog>
+          <AlertDialogTrigger>
+            <MenuItem>apagar</MenuItem>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Você tem certeza que quer apagar essa conta?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Essa ação não poderá ser desfeita. Os dados da conta serão perdidos.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>cancelar</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDelete} variant='destructive'>
+                sim, quero apagar essa conta
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+
+        <MenuItem onClick={handleEdit}>editar</MenuItem>
+
       </Menu>
     </div>
   );
